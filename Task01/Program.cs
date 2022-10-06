eb13bf70a7fdfad24e4d80c430ad88db31bed5c3
Vitaliy <vitaliy.klimov@mail.ru>
2022-10-07 00:46:37 +0300

    Task03 complete

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -6,12 +6,11 @@
 // a = 9, b = -3 -> да
 // a = -3 b = 9 -> нет
 
-Console.Write ("Введите число 1: ");
-int num1 = Convert.ToInt32(Console.ReadLine ());
-Console.Write ("Введите число 2: ");
-int num2 = Convert.ToInt32(Console.ReadLine ());
+Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
 if (num1 == num2 * num2)
-{
-    Console.WriteLine ("Да");
-}
-    else Console.WriteLine ("нет");
+    Console.WriteLine("Да");
+else Console.WriteLine("нет");
