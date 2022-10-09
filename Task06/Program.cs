78333c0018131f3ca2a9dfd55858426f2265713e
Vitaliy <vitaliy.klimov@mail.ru>
2022-10-09 18:45:00 +0300

    Task08 complete

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -10,6 +10,6 @@ int num = Convert.ToInt32(Console.ReadLine());
 
 if (num % 2 == 0)
    
-    Console.WriteLine("Четное");
+    Console.WriteLine("Да");
 
-else Console.WriteLine("Нечетное");
+else Console.WriteLine("Нет");
