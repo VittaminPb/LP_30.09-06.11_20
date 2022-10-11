4294dedab2a12080d094990b14319ab0ff60e9c3
Vitaliy <vitaliy.klimov@mail.ru>
2022-10-11 22:53:09 +0300

    Task11 complete

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -9,11 +9,25 @@
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из диапазона 10 - 99 => {number}");
 
-int firstDigit = number / 10; // 78 / 10 = 7
-int secondDigit = number % 10; // 78 % 10 = 8
-if (firstDigit == secondDigit) Console.WriteLine("Цифры одинаковы");
-else if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа = {firstDigit}");
-else Console.WriteLine($"Наибольшая цифра числа = {secondDigit}");
+// int firstDigit = number / 10; // 78 / 10 = 7
+// int secondDigit = number % 10; // 78 % 10 = 8
+// if (firstDigit == secondDigit) Console.WriteLine("Цифры одинаковы");
+// else if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа = {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа = {secondDigit}");
 
 // int res = Math.Max(firstDigit, secondDigit);
 // Console.WriteLine($"Наибольшая цифра числа = {res}");
+
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10; // 78 / 10 = 7
+    int secondDigit = num % 10; // 78 % 10 = 8
+    // if (firstDigit > secondDigit) return firstDigit;
+    // return secondDigit;
+
+    return firstDigit > secondDigit ? firstDigit : secondDigit; // --- ! --- Тернарный оператор--- ! ---
+}
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа = {maxDigit}");
---
+++ Task11/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из диапазона 1 - 999 => {number}");
+
+// int firstDigit = number / 100; // 456 / 100 = 4
+// int thirdDigit = number % 10; // 456 % 10 = 6
+// int splitNumber = firstDigit * 10 + thirdDigit;
+
+// Console.WriteLine($"Число из крайних цифер => {splitNumber}");
+
+int SplitNumber(int num)
+{
+    int firstDigit = number / 100; // 456 / 100 = 4
+    int thirdDigit = number % 10;
+    int splitNumber = firstDigit * 10 + thirdDigit;
+    return splitNumber;
+}
+
+int splitNumber = SplitNumber(number);
+Console.WriteLine($"Число из крайних цифер => {splitNumber}");
