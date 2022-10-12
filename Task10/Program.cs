383806a41d97d36393b0dcbcbd817f60234a5138
Vitaliy <vitaliy.klimov@mail.ru>
2022-10-12 22:49:26 +0300

    Task15 complete

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -10,7 +10,7 @@
 Console.Write("Введите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if (num < 100 || num >999)
+if (num < 100 || num > 999)
 
     Console.Write("Число не трехзначное!");
     
---
+++ Task15/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите номер дня недели: ");
+int numDay = Convert.ToInt32(Console.ReadLine());
+
+// switch (numDay)          // можно взять за основу решенную нами задачу с использованием конструкции "swith case"
+// {
+//     case 1:
+//         Console.WriteLine("Нет");
+//         break;
+//     case 2:
+//         Console.WriteLine("Нет");
+//         break;
+//     case 3:
+//         Console.WriteLine("Нет");
+//         break;
+//     case 4:
+//         Console.WriteLine("Нет");
+//         break;
+//     case 5:
+//         Console.WriteLine("Нет");
+//         break;
+//     case 6:
+//         Console.WriteLine("Да");
+//         break;
+//     case 7:
+//         Console.WriteLine("Да");
+//         break;
+//     default:
+//         Console.WriteLine("Нет такого дня недели");
+//         break;
+// }
+
+if (numDay < 1 || numDay > 7)   // Либо написать свое решение))
+    Console.WriteLine("Нет такого дня недели");
+else if (numDay == 6 || numDay == 7)
+    Console.WriteLine($"{numDay} -> Да");
+else Console.WriteLine($"{numDay} -> Нет");
