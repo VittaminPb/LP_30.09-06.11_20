23ec3f38a8addc742b3ff1a54c6a4026b63a2016
Vitaliy <vitaliy.klimov@mail.ru>
2022-10-12 20:49:10 +0300

    Task14 complete

---
+++ Task14/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 7 == 0 && num %23 == 0)            // решение через if else
+//     Console.WriteLine($"{num} -> да");
+// else Console.WriteLine($"{num} -> нет");
+
+bool CheckNum(int number)
+{
+    return number % 7 == 0 && number % 23 == 0;
+}
+
+bool result = CheckNum(num);
+if (result)
+    Console.WriteLine($"{num} -> да");
+else Console.WriteLine($"{num} -> нет");
