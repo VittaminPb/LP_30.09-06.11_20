17758ec10c86ee4444dfff77e61c2ed78d75ba4c
Vitaliy <vitaliy.klimov@mail.ru>
2022-10-12 21:24:21 +0300

    Task16 complete

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -6,3 +6,20 @@
 // 25, 5 -> да
 // 8,9 -> нет
 
+bool CheckNum(int number1, int number2)
+{
+    return number1 * number1 == number2 || number2 * number2 == number1;
+}
+
+Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = CheckNum(num1, num2);
+
+if (result)
+
+    Console.WriteLine($"{num1}, {num2} -> да");
+
+else Console.WriteLine($"{num1}, {num2} -> нет");
