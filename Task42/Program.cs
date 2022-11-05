d479e35c415b674a1028ed06eb949d65d84ab5ce
Vitaliy <vitaliy.klimov@mail.ru>
2022-11-05 13:05:06 +0300

    Task42 complete

---
+++ Task42/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Binary(int num)
+{
+    int tmp = 0;
+    int count = 1;
+    while (num > 0)
+    {
+        tmp = tmp + num % 2 * count;
+        num = num / 2;
+        count = count * 10;
+    }
+    return tmp;
+}
+
+int binary = Binary(number);
+Console.WriteLine($"{number} -> {binary}");
