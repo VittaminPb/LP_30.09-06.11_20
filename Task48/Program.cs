94b59777790dc0c96341f423bb4009164d339313
Vitaliy <vitaliy.klimov@mail.ru>
2022-11-07 22:51:09 +0300

    Task48 complete

--- Task48/Program.cs
+++ Task48/Program.cs
@@ -1,22 +1,21 @@
-﻿// Задача 46: Задайте двумерный массив размером m×n,
-// заполненный случайными целыми числами.
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Amn = m+n.
+// Выведите полученный массив на экран.
 // m = 3, n = 4.
-// 1 4 8 19
-// 5 -2 33 -2
-// 77 3 8 1
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
 
-int[,] CreateMatrixRndInt(int rows, int columns, int min, int max) // создание двумерного массива rnd
+int[,] CreateMatrixRndInt(int rows, int columns)
 {
     var matrix = new int[rows, columns];
-    var rnd = new Random();
-
+    
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = rnd.Next(min, max + 1);
+            matrix[i, j] = i + j;
         }
-
     }
     return matrix;
 }
@@ -35,5 +34,5 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
-int[,] array2D = CreateMatrixRndInt(3, 4, -100, 100);
+int[,] array2D = CreateMatrixRndInt(3, 4);
 PrintMatrix(array2D);
